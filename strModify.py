18343dbdce5805b62ac88e366f190cfbc34f5783
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2023-01-17 15:59:41 +0330

    string methods

---
+++ strModify.py
@@ -0,0 +1,6 @@
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+print(a.strip()) # remove whitespace from the beginning or the end.
+print(a.replace("H", "J")) # find char in first input and replace it with second input char
+print(a.split(",")) # returns ['Hello', ' World!']
