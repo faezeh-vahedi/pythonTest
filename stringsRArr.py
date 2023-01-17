f37d985c465bfaa8a58809ad4b81a66142bc17a6
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2023-01-17 15:41:45 +0330

    string methods

---
+++ stringsRArr.py
@@ -0,0 +1,15 @@
+a = "Hello world"
+print(a[2])
+
+for x in "banana":
+    print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+
+txt = "The best things in life are free!"
+print("fre" in txt)
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
