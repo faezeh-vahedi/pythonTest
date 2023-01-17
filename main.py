66234d5e201a4def98df049a16f07c717c45d8dc
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2023-01-17 15:34:45 +0330

    add random slot

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 
 
-# Float can also be scientific numbers with an "e" to indicate the power of 10.
+# ! Float can also be scientific numbers with an "e" to indicate the power of 10.
+# ? Note: You cannot convert complex numbers into another number type.
