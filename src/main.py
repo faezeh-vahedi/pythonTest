c1a7d2a2f6659341b13e9714675d46a2b97089b8
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2023-01-18 14:53:25 +0330

    dockerize the project & change dir

--- src/main.py
+++ src/main.py
@@ -2,3 +2,6 @@
 
 # ! Float can also be scientific numbers with an "e" to indicate the power of 10.
 # ? Note: You cannot convert complex numbers into another number type.
+# Sample taken from pyStrich GitHub repository
+# https://github.com/mmulqueen/pyStrich
+print("Hello world!")
